ed3f79d463d14ae0ff5bb5db949c435deb2f536e
Netson <netsoncavina@gmail.com>
2022-03-25 20:46:03 -0300

    Adicionado algoritmo

---
+++ countingSort.js
@@ -0,0 +1,32 @@
+function countingSort(array) {
+  let counts = [];
+
+  for (n of array) {
+    if (counts[n]) {
+      counts[n]++;
+    } else {
+      counts[n] = 1;
+    }
+  }
+
+  let result = [];
+
+  //   Metodo lento para numeros grandes
+  //   for (let i = 0; i < counts.length; i++) {
+  //     for (let j = 0; j < counts[i]; j++) {
+  //       result.push(i);
+  //     }
+  //   }
+
+  //   Metodo rapido para numeros grandes
+  for (key of Object.keys(counts)) {
+    for (let j = 0; j < counts[key]; j++) {
+      result.push(Number(key));
+    }
+  }
+
+  return result;
+}
+
+let array = [1, 4, 1, 2, 7, 5, 99999999];
+console.log(countingSort(array));
